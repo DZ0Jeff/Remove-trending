b15582cb2a275387b969190f87852fea74780632
Jeferson <mx,jeferson.12@gmail.com>
2020-04-27 00:39:19 -0300

    0.2

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
  * @author: DZ0Jeff
  */
 
-try {
-    setTimeout(() => {
-        const trends = document.querySelector('.css-1dbjc4n.r-1uaug3w.r-1uhd6vh.r-t23y2h.r-1phboty.r-rs99b7.r-ku1wi2.r-1udh08x')
+const timer = setInterval(() => {
+    const trends = document.querySelector('.css-1dbjc4n.r-1uaug3w.r-1uhd6vh.r-t23y2h.r-1phboty.r-rs99b7.r-ku1wi2.r-1udh08x')
 
+    if(trends) {
+        clearInterval(timer)
+        console.log(trends)
         trends.parentNode.removeChild(trends)
-    },3000)
-
-} catch(err) {
-    console.error(err)
-}
+    }
+},1000)
